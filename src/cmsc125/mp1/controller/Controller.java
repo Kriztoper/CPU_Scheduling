2019fcee5b44c4345d0f2049ab175743ab083164
Kriztoper Urmeneta <kriztoperurmeneta@gmail.com>
2017-04-10 09:48:50 +0800

    Added constructors to main, controller, view

--- src/cmsc125/mp1/controller/Controller.java
+++ src/cmsc125/mp1/controller/Controller.java
@@ -1,5 +1,17 @@
 package cmsc125.mp1.controller;
 
+import cmsc125.mp1.view.View;
+
 public class Controller {
 
+	private View view;
+	
+	public Controller(View view) {
+		this.view = view;
+		
+	}
+	
+	public void start() {
+		
+	}
 }
