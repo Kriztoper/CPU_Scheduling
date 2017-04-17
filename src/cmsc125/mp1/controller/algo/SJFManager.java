c5f2a1f8e2dff6440f797db83e338256d8c10cfe
Kriztoper Urmeneta <kriztoperurmeneta@gmail.com>
2017-04-17 12:49:41 +0800

    Added readyQueue to SJFManager

--- src/cmsc125/mp1/controller/algo/SJFManager.java
+++ src/cmsc125/mp1/controller/algo/SJFManager.java
@@ -2,6 +2,7 @@ package cmsc125.mp1.controller.algo;
 
 import javax.swing.JTable;
 
+import cmsc125.mp1.controller.datastructures.ProcessesQueue;
 import cmsc125.mp1.view.panels.SimulationPanel;
 
 public class SJFManager extends Thread {
@@ -9,16 +10,19 @@ public class SJFManager extends Thread {
 	private SimulationPanel simulationPanel;
 	private JTable resourcesTable;
 	private JTable timeTable;
+	private ProcessesQueue readyQueue;
 	
 	public SJFManager(SimulationPanel simulationPanel, 
 			JTable resourcesTable, JTable timeTable) {
 		this.simulationPanel = simulationPanel;
 		this.resourcesTable = resourcesTable;
 		this.timeTable = timeTable;
+		readyQueue = new ProcessesQueue();
 	}
 
 	public void startSimulation() {
-		
+		//initProcessesInVector();
+		//sortProcessesToReadyQueue();
 		
 		start();
 	}
