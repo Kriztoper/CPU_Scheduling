3870e84442afa0938e163d1daea77f491c6e3c38
Noel Garcia <nagarcia2@up.edu.ph>
2017-06-05 09:54:45 +0800

    Disk Scheduling Algorithms and Sample Run
    * SCAN and CSCAN needs to be checked
    * includes total head movement
    * result is queue for disk traversal

---
+++ src/cmsc125/mp1/algorithms/disk/FCFS.java
@@ -0,0 +1,18 @@
+package cmsc125.mp1.algorithms.disk;
+
+import java.util.Queue;
+
+public class FCFS extends DiskScheduling{
+	
+	public FCFS(int currentPiece, int maxPiece) {
+		super(currentPiece, maxPiece);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Queue<Integer> process() {	
+		result = (Queue<Integer>) pieces.clone();
+		return result;
+	}
+
+}
